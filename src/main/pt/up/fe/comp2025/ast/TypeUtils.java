ec90ebf14c0f6d01909e1f382aa6e5c265cdb87a
nelsonjoneto <up202108117@up.pt>
2025-03-15 19:56:16 +0000

    Passing varargs

--- src/main/pt/up/fe/comp2025/ast/TypeUtils.java
+++ src/main/pt/up/fe/comp2025/ast/TypeUtils.java
@@ -18,7 +18,6 @@ import static pt.up.fe.comp2025.ast.Kind.METHOD_DECL;
  */
 public class TypeUtils {
 
-
     private final JmmSymbolTable table;
 
     public TypeUtils(SymbolTable table) {
@@ -36,28 +35,17 @@ public class TypeUtils {
     public static Type newStringType() {
         return new Type("String", false);
     }
-    
-    public static Type convertType(JmmNode typeNode) {
 
-        // TODO: When you support new types, this must be updated
-        // DONE: Updated to support new types
+    public static Type convertType(JmmNode typeNode) {
         String name = typeNode.get("name");
         boolean isArray = Boolean.parseBoolean(typeNode.get("isArray"));
-
         return new Type(name, isArray);
     }
 
-
     /**
      * Gets the {@link Type} of an arbitrary expression.
-     *
-     * @param expr
-     * @return
      */
     public Type getExprType(JmmNode expr) {
-
-        // TODO: Update when there are new types
-        // DONE: Updated to support new types
         switch (Kind.fromString(expr.getKind())) {
             case PARENT_EXPR:
                 return getExprType(expr.getChild(0));
@@ -69,9 +57,7 @@ public class TypeUtils {
                     return new Type(className, false);
                 }
 
-                if(table.getImports().stream()
-                        .flatMap(importName -> Arrays.stream(importName.substring(1, importName.length() - 1).split(",")))
-                        .anyMatch(importName -> importName.trim().equals(className))){
+                if (isImported(className)) {
                     return new Type(className, false);
                 }
 
@@ -92,6 +78,12 @@ public class TypeUtils {
                 Type objectType = getExprType(expr.getChild(0));
                 String methodName = expr.get("name");
 
+                // Handle imported classes
+                if (isImported(objectType.getName())) {
+                    return new Type("Object", false); // Assume valid return type for imported classes
+                }
+
+                // Handle local methods
                 Type returnType = table.getReturnType(methodName);
                 if (returnType == null) {
                     throw new RuntimeException("Method '" + methodName + "' not found in class " + objectType.getName());
@@ -107,7 +99,6 @@ public class TypeUtils {
 
             case BINARY_EXPR: {
                 String op = expr.get("op");
-
                 switch (op) {
                     case "*","/","+","-":
                         return newIntType();
@@ -121,65 +112,54 @@ public class TypeUtils {
             case VAR_REF_EXPR:
                 String currentMethod = expr.getAncestor(METHOD_DECL)
                         .map(node -> node.get("name"))
-                        .orElseThrow(() -> new RuntimeException("Cannot determine the current method for variable: " + expr.get("name")));
-
+                        .orElseThrow(() -> new RuntimeException("Cannot determine current method for variable: " + expr.get("name")));
                 return resolveVariableType(expr.get("name"), currentMethod);
 
-            case ARRAY_INIT, NEW_INT_ARRAY_EXPR: {
+            case ARRAY_INIT, NEW_INT_ARRAY_EXPR:
                 return new Type("int", true);
-            }
 
             default:
                 throw new UnsupportedOperationException("Unhandled expression type: " + expr.getKind());
         }
     }
 
+    private boolean isImported(String className) {
+        return table.getImports().stream()
+                .flatMap(importName -> Arrays.stream(importName.substring(1, importName.length() - 1).split(",")))
+                .map(String::trim)
+                .anyMatch(importName -> importName.equals(className));
+    }
+
     private Type resolveVariableType(String varName, String currentMethod) {
-        // Check local variables of the current method
+        // Check local variables
         Optional<Type> localType = table.getLocalVariables(currentMethod).stream()
                 .filter(local -> local.getName().equals(varName))
                 .findFirst()
                 .map(Symbol::getType);
 
-        if (localType.isPresent()) {
-            //System.out.println("Resolved as local variable: " + varName + " -> " + localType.get().getName());
-            return localType.get();
-        }
+        if (localType.isPresent()) return localType.get();
 
-        // Check parameters of the current method
+        // Check parameters
         Optional<Type> paramType = table.getParameters(currentMethod).stream()
                 .filter(param -> param.getName().equals(varName))
                 .findFirst()
                 .map(Symbol::getType);
 
-        if (paramType.isPresent()) {
-            //System.out.println("Resolved as parameter: " + varName + " -> " + paramType.get().getName());
-            return paramType.get();
-        }
+        if (paramType.isPresent()) return paramType.get();
 
-        // Check fields (class-level variables)
+        // Check fields
         Optional<Type> fieldType = table.getFields().stream()
                 .filter(field -> field.getName().equals(varName))
                 .findFirst()
                 .map(Symbol::getType);
 
-        if (fieldType.isPresent()) {
-            //System.out.println("Resolved as field: " + varName + " -> " + fieldType.get().getName());
-            return fieldType.get();
-        }
+        if (fieldType.isPresent()) return fieldType.get();
 
-        // Check if the variable matches an imported class
-        Optional<String> importedClass = table.getImports().stream()
-                .flatMap(importName -> Arrays.stream(importName.substring(1, importName.length() - 1).split(",")))
-                .map(String::trim)
-                .filter(imported -> imported.equals(varName))
-                .findFirst();
-
-        if (importedClass.isPresent()) {
+        // Check imports
+        if (isImported(varName)) {
             return new Type(varName, false);
         }
 
         throw new RuntimeException("Variable '" + varName + "' not found");
     }
-
 }
