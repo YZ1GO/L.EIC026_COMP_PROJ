7e8c5c83e466612c9c967a766a56f988dfc2d12d
Bruno Huang <brunohuang2012@gmail.com>
2025-05-20 09:28:41 +0100

    fix: ClassNameConflictChecker wrongly comparing class name with directory instead of import (last section after .)

--- test/pt/up/fe/comp/customTests/OllirGenerationTest.java
+++ test/pt/up/fe/comp/customTests/OllirGenerationTest.java
@@ -24,11 +24,13 @@ public class OllirGenerationTest {
         System.out.println("Generated OLLIR:");
         System.out.println(result.getOllirCode());
 
-        if (!result.getOllirCode().trim().replaceAll("\\s+", " ")
+        assertEquals(result.getOllirCode().trim().replaceAll("\\s+", " "), expectedOllir.trim().replaceAll("\\s+", " "));
+
+        /*if (!result.getOllirCode().trim().replaceAll("\\s+", " ")
                 .equals(expectedOllir.trim().replaceAll("\\s+", " "))) {
             System.out.println("OLLIR output does not match expected result.");
             return false;
-        }
+        }*/
         return true;
     }
 
@@ -410,6 +412,22 @@ public class OllirGenerationTest {
     public void testArrayField4() {
         assertTrue(testOllirGeneration("ArrayField4.jmm", "ArrayField4.ollir"));
         OllirResult ollirResult = getOllirResult("ArrayField4.jmm");
+        System.out.println("Generated OLLIR:");
+        System.out.println(ollirResult.getOllirCode());
+    }
+
+    @Test
+    public void testImportsFeedback() {
+        assertTrue(testOllirGeneration("Imports.jmm", "Imports.ollir"));
+        OllirResult ollirResult = getOllirResult("Imports.jmm");
+        System.out.println("Generated OLLIR:");
+        System.out.println(ollirResult.getOllirCode());
+    }
+
+    @Test
+    public void testImportsFeedback2() {
+        assertTrue(testOllirGeneration("Imports2.jmm", "Imports2.ollir"));
+        OllirResult ollirResult = getOllirResult("Imports2.jmm");
         System.out.println("Generated OLLIR:");
         System.out.println(ollirResult.getOllirCode());
     }
